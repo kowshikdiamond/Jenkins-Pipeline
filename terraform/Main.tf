9bfbce16dcb0255b8675dc0948b925d88415e1bb
kowshikdiamond <kowshikdiamonds@gmail.com>
2023-11-20 11:57:24 +0530

    output public ip address of created server

--- terraform/Main.tf
+++ terraform/Main.tf
@@ -10,13 +10,17 @@ data "aws_security_group" "existing" {
   name = "ec2-ssh"
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "deployFromJenkins" {
   ami           = "ami-0287a05f0ef0e9d9a"  # Use a valid AMI ID for your desired operating system
   instance_type = "t2.micro"  # Set your desired instance type
   key_name      = data.aws_key_pair.existing.key_name
   vpc_security_group_ids = [data.aws_security_group.existing.id]
 
   tags = {
-    Name = "example-instance"
+    Name = "test"
   }
+}
+
+output "ec2_public_ip" {
+    value = aws_instance.deployFromJenkins.public_ip
 }
